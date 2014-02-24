4ab4c428cf7aeae801a87c767c51fe03b87e5ccd
imagentleman <imagentlemail@gmail.com>
2014-02-24 17:10:34 -0500

    Create config.php

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+$cfg['controller_main'] = ''; // Default controller (for the route "/")
+$cfg['view_error'] = ''; // Default view for 404 errors
+
+$db['adapter'] = 'mysql'; // The pdo driver (e.g. "mysql")
+$db['host'] = '';
+$db['name'] = ''; // DB name
+$db['user'] = ''; // User name
+$db['password'] = '';
